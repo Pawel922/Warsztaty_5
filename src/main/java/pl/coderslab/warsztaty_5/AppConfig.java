2c466ee933eac9cb32fae45ff58f8c094892e34d
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-03-31 23:16:26 +0200

    AppConfig and AppInitializer were added

---
+++ src/main/java/pl/coderslab/warsztaty_5/AppConfig.java
@@ -0,0 +1,20 @@
+package pl.coderslab.warsztaty_5;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class AppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("http://localhost");
+    }
+}
