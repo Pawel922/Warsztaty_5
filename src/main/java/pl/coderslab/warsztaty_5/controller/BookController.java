c5e8375b12ead4db0afe3d66726bc94666e7b934
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-04-01 23:03:12 +0200

    New attribute was added to BookController

--- src/main/java/pl/coderslab/warsztaty_5/controller/BookController.java
+++ src/main/java/pl/coderslab/warsztaty_5/controller/BookController.java
@@ -1,12 +1,22 @@
 package pl.coderslab.warsztaty_5.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.coderslab.warsztaty_5.service.BookService;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
     @GetMapping("/hello")
     public String hello(){
         return "Witaj na 5 warsztacie";
