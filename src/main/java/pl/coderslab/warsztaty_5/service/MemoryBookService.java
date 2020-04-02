0d53e0fcc2e69aae5d9a2c90803619146ba67830
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-04-02 21:05:50 +0200

    New attribute was added and constructor to initialize list of books

--- src/main/java/pl/coderslab/warsztaty_5/service/MemoryBookService.java
+++ src/main/java/pl/coderslab/warsztaty_5/service/MemoryBookService.java
@@ -3,14 +3,27 @@ package pl.coderslab.warsztaty_5.service;
 import org.springframework.stereotype.Component;
 import pl.coderslab.warsztaty_5.model.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class MemoryBookService implements BookService {
 
+    private List<Book> books;
+
+    public MemoryBookService() {
+        books = new ArrayList<>();
+        books.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel",
+                "Helion", "programming"));
+        books.add(new Book(2L, "9788324627738", "Rusz glowa, Java.",
+                "Sierra Kathy, Bates Bert", "Helion", "programming"));
+        books.add(new Book(3L, "9780130819338", "Java 2. Podstawy",
+                "Cay Horstmann, Gary Cornell", "Helion", "programming"));
+    }
+
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return books;
     }
 
     @Override
