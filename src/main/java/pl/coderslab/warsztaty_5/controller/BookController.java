5876d820bf551b7b762af791dec0b7b6ed6630d6
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-04-02 21:25:31 +0200

    New address was implemented to show book with concrete id

--- src/main/java/pl/coderslab/warsztaty_5/controller/BookController.java
+++ src/main/java/pl/coderslab/warsztaty_5/controller/BookController.java
@@ -2,6 +2,7 @@ package pl.coderslab.warsztaty_5.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.warsztaty_5.model.Book;
@@ -23,5 +24,11 @@ public class BookController {
     @GetMapping
     public List<Book> books() {
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book bookById(@PathVariable long id) {
+        Book book = bookService.getBookById(id);
+        return book;
     }
 }
